b533d2777b744c979f52d26e428374d8399afec9
Theparewa140347 <108443809+Theparewa140347@users.noreply.github.com>
2024-04-17 13:38:33 +0700

    profile

---
+++ constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kprimaryColor = Color.fromARGB(255, 144, 162, 197);
+const ksecondryColor = Color(0xff9CA2FF);
+const ksecondryLightColor = Color.fromARGB(255, 122, 183, 207);
+const klightContentColor = Color.fromARGB(255, 216, 226, 151);
+
+const double kbigFontSize = 25;
+const double knormalFontSize = 18;
+const double ksmallFontSize = 15;
